68879b69b754ba1ec109a097ba3a9c1cf121ae4e
Kenan William <kenanallanwilliam@gmail.com>
2026-04-05 13:44:08 +0300

    fixed node issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-};
+  outputFileTracingRoot: undefined,
+  experimental: {
+    turbo: undefined,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
