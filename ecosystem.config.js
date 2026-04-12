dce52f7ba6b83e21dd9ffbe436f248d7db8a52ad
Kenan William <kenanallanwilliam@gmail.com>
2026-04-12 23:34:14 +0300

    chore: reset to stable server.js configuration with headline swap

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
   apps: [
     {
       name: 'envecoplast',
-      script: './app.js',
+      script: './server.js',
       instances: 1,
       exec_mode: 'cluster',
       env: {
