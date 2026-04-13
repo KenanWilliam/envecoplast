b73466ca5b05055d8673e866fb4fca55d73533f6
Kenan William <kenanallanwilliam@gmail.com>
2026-04-13 17:42:03 +0300

    content: swapped home page heading and description and updated main headline

--- lib/site.ts
+++ lib/site.ts
@@ -2,10 +2,9 @@ export const site = {
   name: 'Envecoplast',
   legalName: 'Envecoplast Company Limited',
   tagline: 'Turning waste into worth - building a greener future',
-  description:
-    'Envecoplast Ltd  is a forward-thinking plastic recycling and manufacturing company  that sells traceable pre-processed, recycled plastics from the environment and further transforms it into  high-quality construction materials.',
+  description: 'Turning waste into worth - building a greener future',
   headline:
-    'Envecoplast Ltd sells traceable pre-processed, recycled plastics from the environment and transforms them into high-quality construction materials.',
+    'Envecoplast Ltd sells traceable pre-processed recycled plastics from the land fills and transforms them into high quality plastic chips and pellets',
   location: 'Nairobi, Kenya',
   locationDetailed: 'Industrial Area, Nairobi, Kenya',
   formspreeEndpoint: process.env.NEXT_PUBLIC_FORM_ENDPOINT || 'https://formspree.io/f/xblypvvz',
